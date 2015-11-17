a7f7acff65061228278a5581610bbc0e4c09d6dc
sx4452 <zb.benzhang@gmail.com>
2015-11-17 19:41:18 +0800

    20151117

--- DMAS4_PegasosAlgorithm.m
+++ DMAS4_PegasosAlgorithm.m
@@ -3,22 +3,21 @@ train1 = load('dataset1-a8a-training.txt','rt');
 test1 = load('dataset1-a8a-testing.txt','rt');
 train2 = load('dataset1-a9a-training.txt','rt');
 test2 = load('dataset1-a9a-testing.txt','rt');
-b = 0;
 lamda1 = 10^-4;
 lamda2 = 5*10^-5;
-
 %[x1h, y1h] = PegasosAlgorithmhinge(train1, lamda1, test1);
 %[x2h, y2h] = PegasosAlgorithmhinge(train2, lamda2, test2);
-[x1l, y1l] = PegasosAlgorithmlog(train1, lamda1, test1);
-%[xl2, y2l] = PegasosAlgorithmlog(train2, lamda2, test2);
-labelh1 = 'hinge-loss test1';
-labelh2 = 'hinge-loss test2';
-labell1 = 'log-loss test1';
+%[x1l, y1l] = PegasosAlgorithmlog(train1, lamda1, test1);
+[x2l, y2l] = PegasosAlgorithmlog(train2, lamda2, test2);
+%labelh1 = 'hinge-loss test1';
+%labelh2 = 'hinge-loss test2';
+%labell1 = 'log-loss test1';
 labell2 = 'log-loss test2';
-Output(x1l, y1l, labell1);
-
+%Output(x1h, y1h, labelh1);
+%Output(x2h, y2h, labelh2);
+%Output(x1l, y1l, labell1);
+Output(x2l, y2l, labell2);
 end
-
 
 function [resultX, resultY] = PegasosAlgorithmhinge(S, lamda, test)
 resultX = zeros(1,10);
@@ -30,7 +29,7 @@ Wout = zeros(1,n-1);
 for t = 1:T
     i = randperm(m, 1);
     eta = 1/(lamda*t);
-    if S(i,n) < 1
+    if Wout*S(i,1:n-1)'*S(i,n) < 1
         Wout = (1 - eta*lamda)*Wout + eta*S(i, n)*S(i,1:n-1);
     else
         Wout = (1 - eta*lamda)*Wout;
@@ -54,8 +53,8 @@ for t = 1:T
     i = randperm(m, 1);
     eta = 1/(lamda*t);
     yi = S(i,n);
-    Z = sum(Wout.*S(i,1:n-1));
-    Wout = Wout - eta*-(yi/(1+exp(yi*Z))*S(i,1:n-1));
+    Z = Wout*S(i,1:n-1)';
+    Wout = (1 - eta*lamda)*Wout + eta*yi/(1+exp(yi*Z))*S(i,1:n-1);
     if t == round(step*T)
         resultX(round(step*10)) = step;
         resultY(round(step*10)) = TestPegasos(Wout, test);
@@ -84,6 +83,7 @@ testerror = testerror / m;
 end
 
 function Output(x, y, label)
+disp(y);
 plot(x, y);
 set(gca,'ytick',0:0.1:1);
 axis([0 1 0 1]);
